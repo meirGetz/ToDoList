fd9073f325c2497a0df600b86e37d2de7658dbba
meirGetz <meirgetz@gmail.com>
2024-12-06 09:10:38 +0200

    create postMapping of create user

--- src/main/java/com/task/controllers/UserController.java
+++ src/main/java/com/task/controllers/UserController.java
@@ -1,2 +1,42 @@
-package com.task.controllers;public class UserController {
+package com.task.controllers;
+
+import com.task.DTO.ListObjectRequest;
+import com.task.Service.UserService;
+import com.task.entities.ListObject;
+//import com.task.entities.TaskTable;
+import com.task.entities.Users;
+import com.task.repositories.TaskRepository;
+//import com.task.repositories.TaskTableRepository;
+import com.task.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/createUser")
+    public ResponseEntity<Users> createUser(@RequestBody Users request) {
+        Users createdUser = userService.createUser(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
 }
+
+
+
