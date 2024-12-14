ca8d677da8c89f38d85b47413f2bdda8d80b4b27
meirGetz <meirgetz@gmail.com>
2024-12-14 21:15:22 +0200

    Implement CustomUserDetailsService for user authentication

--- src/main/java/com/task/Security/CustomUserDetailsService.java
+++ src/main/java/com/task/Security/CustomUserDetailsService.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+import org.springframework.ui.Model;
 import com.task.entities.Users;
 import com.task.repositories.UserRepository;
 
@@ -18,39 +18,25 @@ import org.slf4j.LoggerFactory;
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
     private static final Logger logger = LoggerFactory.getLogger(CustomUserDetailsService.class);
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        logger.info("Received username: {}", username);
-        Users user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-        return new User(
-                user.getUsername(),
-                user.getPassword(),
-                user.getRoles().stream()
-                        .map(role -> (GrantedAuthority) () -> "ROLE_" + role)
-                        .collect(Collectors.toList())
-        );
-    }
-
-
     private final UserRepository userRepository;
 
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Users user = userRepository.findByUsername(username)
-//                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-//
-//        return new User(
-//                user.getUsername(),
-//                user.getPassword(),
-//                user.getRoles().stream()
-//                        .map(role -> (GrantedAuthority) () -> "ROLE_" + role)
-//                        .collect(Collectors.toList())
-//        );
-//    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Users user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+        return org.springframework.security.core.userdetails.User.withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+
+
+
+
+
 }
