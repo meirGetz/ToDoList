56a14c1e5200899ba8eb1a1d1e7825753ec27d0d
meirGetz <meirgetz@gmail.com>
2024-12-30 10:19:27 +0200

    Adding a note class to the program run

--- Task-service/src/main/java/com/task/TaskServiceApplication.java
+++ Task-service/src/main/java/com/task/TaskServiceApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@EnableJpaRepositories(basePackages = "com.task.repositories")
-@EntityScan(basePackages = "com.task.entities")
-@SpringBootApplication(scanBasePackages = {"com.task", "com.user"})
-
+@EnableJpaRepositories(basePackages = {"com.task.repositories", "com.task.note.repositories"})
+@EntityScan(basePackages = {"com.task.entities","com.task.note.entities"})
+@SpringBootApplication(scanBasePackages = {"com.task", "com.user", "com.task.note"})
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskServiceApplication.class, args);
     }
 }
+
