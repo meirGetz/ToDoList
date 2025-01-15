67892eebbe8ac7ffe8c95d196fa7949ecb4716d2
meirGetz <meirgetz@gmail.com>
2025-01-16 00:31:30 +0200

    Moving two classes to a new module

--- Common-module/src/main/java/com/DTO/UserDto.java
+++ Common-module/src/main/java/com/DTO/UserDto.java
@@ -1,4 +1,4 @@
-package com.user.DTO;
+package com.DTO;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -7,11 +7,13 @@ public class UserDto {
     @NotNull
     @NotEmpty
     private String firstName;
-
+    @NotNull
+    @NotEmpty
+    private long id;
     @NotNull
     @NotEmpty
     private String lastName;
-@NotNull
+    @NotNull
     @NotEmpty
     private String role;
 
@@ -27,47 +29,66 @@ public class UserDto {
     @NotEmpty
     private String phone;
 
-    public String getRole(){
+    public String getRole() {
         return role;
     }
-    public void setRole(String role){
+
+    public void setRole(String role) {
         this.role = role;
     }
+
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getMatchingPassword() {
         return matchingPassword;
     }
+
     public void setMatchingPassword(String matchingPassword) {
         this.matchingPassword = matchingPassword;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public long getId() {
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
 }
